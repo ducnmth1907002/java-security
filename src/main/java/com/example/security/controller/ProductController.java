94244e0697dc78e51425e8c9cc158f5c1778105f
Your Name <ducnmth1907002@fpt.edu.vn>
2021-06-21 21:18:27 +0700

    update

--- src/main/java/com/example/security/controller/ProductController.java
+++ src/main/java/com/example/security/controller/ProductController.java
@@ -3,12 +3,14 @@ package com.example.security.controller;
 import com.example.security.entity.Product;
 import com.example.security.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping
 public class ProductController {
